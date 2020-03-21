5e812c0d34e3c8f1fbc1f3f0a9b63a0581d654f5
nogrethumphrey@gmail.com <nogrethumphrey@gmail.com>
2020-03-21 20:19:44 +0100

    initial commit

---
+++ src/main/java/com/qiusuo/herokuspringdemo/HerokuSpringdemoApplication.java
@@ -0,0 +1,13 @@
+package com.qiusuo.herokuspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuSpringdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuSpringdemoApplication.class, args);
+	}
+
+}
