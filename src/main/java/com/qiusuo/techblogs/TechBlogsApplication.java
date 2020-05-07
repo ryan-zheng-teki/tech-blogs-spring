d65c624cb045edadd76520fe7c5b66031b8959fa
nogrethumphrey@gmail.com <nogrethumphrey@gmail.com>
2020-05-07 11:21:24 +0000

    remove trivial test code

--- src/main/java/com/qiusuo/techblogs/TechBlogsApplication.java
+++ src/main/java/com/qiusuo/techblogs/TechBlogsApplication.java
@@ -3,15 +3,10 @@ package com.qiusuo.techblogs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 @SpringBootApplication
 public class TechBlogsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TechBlogsApplication.class, args);
-		Path currentWorkingDir = Paths.get("").toAbsolutePath();
-		System.out.println(currentWorkingDir.normalize().toString());
 	}
 }
