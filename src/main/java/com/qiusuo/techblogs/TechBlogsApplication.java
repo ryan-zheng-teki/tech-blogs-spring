c389e63bab4e7611faebeb91c3b97c1e11c09b92
nogrethumphrey@gmail.com <nogrethumphrey@gmail.com>
2020-05-07 15:25:57 +0000

    set mysql properties on prod

--- src/main/java/com/qiusuo/techblogs/TechBlogsApplication.java
+++ src/main/java/com/qiusuo/techblogs/TechBlogsApplication.java
@@ -1,12 +1,16 @@
 package com.qiusuo.techblogs;
 
+import de.dentrassi.crypto.pem.PemKeyStoreProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.security.Security;
 
 @SpringBootApplication
 public class TechBlogsApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		Security.addProvider(new PemKeyStoreProvider());
 		SpringApplication.run(TechBlogsApplication.class, args);
 	}
 }
